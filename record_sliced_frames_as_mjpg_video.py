ec0208d4ecae5048bf2ab5a8c0a05d1148ba5c65
s-tory <custombird@gmail.com>
2024-11-15 14:37:07 +0900

    update.

--- record_sliced_frames_as_mjpg_video.py
+++ record_sliced_frames_as_mjpg_video.py
@@ -17,7 +17,7 @@ num_frames = int(capture_fps*capture_secs)
 width = slicer.camera().width()
 height = slicer.camera().height()
 ch = 3
-frames = np.zeros((num_frames, height, width, ch), np.uint8)
+frames = np.zeros((num_frames, height, width, ch), np.uint8)  # ←メモリ食うよ！
 
 i = 0
 for slice in slicer:
@@ -25,7 +25,7 @@ for slice in slicer:
     print(f"i: {i}, ts: {slice.t}, new slice of {slice.events.size} events")
 
     BaseFrameGenerationAlgorithm.generate_frame(slice.events, frames[i])
-    # cv2.imshow('preview', frames[i])
+    # cv2.imshow('preview', frames[i])  # ←60fps以上を表示させようとすると遅延していく
     # cv2.waitKey(1)
 
     i += 1
@@ -37,9 +37,10 @@ for slice in slicer:
 codec = 'MJPG'
 video_file_path = "./captured.avi"
 video_fps = 60.0
-
 fourcc = cv2.VideoWriter_fourcc(*codec)
 writer = cv2.VideoWriter(video_file_path, fourcc, video_fps, (width, height))
+
 for i in range(num_frames):
     writer.write(frames[i])
+
 writer.release()
