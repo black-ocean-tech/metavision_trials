a71d74f3b07f85810bd8a3fc3d0c9aa8d2008f7a
story <custombird@gmail.com>
2025-01-20 11:10:14 +0900

    update.

---
+++ push_cam_settings_preview.py
@@ -0,0 +1,57 @@
+from metavision_sdk_core import BaseFrameGenerationAlgorithm
+from metavision_sdk_stream import Camera, CameraStreamSlicer, SliceCondition
+from metavision_sdk_ui import EventLoop
+
+camera = Camera.from_first_available()
+
+# fetch, push cam settings
+import json
+
+bias_diff = 300
+bias_diff_off = 150
+bias_diff_on = 450
+
+camera.save("./default_settings.json")
+with open("./default_settings.json", "r") as _:
+    dict_ = json.load(_)
+print(dict_["ll_biases_state"])
+
+dict_["ll_biases_state"]["bias"][0]["value"] = bias_diff
+dict_["ll_biases_state"]["bias"][1]["value"] = bias_diff_off
+dict_["ll_biases_state"]["bias"][2]["value"] = bias_diff_on
+
+with open("./edited_settings.json", "w") as _:
+    json.dump(dict_, _)
+camera.load("./edited_settings.json")
+print(dict_["ll_biases_state"])
+# /fetch, push cam settings
+
+# setup
+import numpy as np
+
+capture_fps = 10000
+
+delta_t_us = int(10**6/capture_fps)
+slice_condition = SliceCondition.make_n_us(delta_t_us)
+slicer = CameraStreamSlicer(camera.move(), slice_condition)
+
+width = slicer.camera().width()
+height = slicer.camera().height()
+frame = np.zeros((height, width, 3), np.uint8)
+# /setup
+
+# preview
+import cv2
+
+for slice in slicer:
+    EventLoop.poll_and_dispatch()
+    print(f"ts: {slice.t}, new slice of {slice.events.size} events")
+
+    BaseFrameGenerationAlgorithm.generate_frame(slice.events, frame)
+    cv2.imshow('preview', frame)
+    cv2.waitKey(1)
+
+# cv2.destroyAllWindows()
+
+# /preview
+
--- record_sliced_frames_as_video_file.py
+++ record_sliced_frames_as_video_file.py
@@ -6,26 +6,26 @@ from metavision_sdk_stream import Camera, CameraStreamSlicer, SliceCondition
 from metavision_sdk_ui import EventLoop
 
 # Setup.
-camera = Camera.from_first_available()
+capture_fps = 5000
+capture_secs = 1.0
 
-capture_fps = 1000
-delta_t_us = int(1000000/capture_fps)
+camera = Camera.from_first_available()
+delta_t_us = int(10**6/capture_fps)
 slice_condition = SliceCondition.make_n_us(delta_t_us)
 slicer = CameraStreamSlicer(camera.move(), slice_condition)
 
-capture_secs = 1.0
 num_frames = int(capture_fps*capture_secs)
 width = slicer.camera().width()
 height = slicer.camera().height()
 ch = 3  # B, G, R
-frames = np.zeros((num_frames, height, width, ch), np.uint8)  # ←メモリ食うよ！
+frames = np.zeros((num_frames, height, width, ch), np.uint8)  # ←動画全キャッシュするのでメモリ食うよ！
 # /Setup.
 
 # Capturing, buffering in mem.
 i = 0
 for slice in slicer:
     EventLoop.poll_and_dispatch()
-    print(f"i: {i}, ts: {slice.t}, new slice of {slice.events.size} events")
+    # print(f"i: {i}, ts: {slice.t}, new slice of {slice.events.size} events")
 
     BaseFrameGenerationAlgorithm.generate_frame(slice.events, frames[i])
     # cv2.imshow('preview', frames[i])  # ←60fps以上を表示させようとすると遅延していく
@@ -42,6 +42,7 @@ for slice in slicer:
 codec = 'H264'
 video_file_path = "./captured.mp4"
 video_fps = 60.0
+
 fourcc = cv2.VideoWriter_fourcc(*codec)
 writer = cv2.VideoWriter(video_file_path, fourcc, video_fps, (width, height))
 
